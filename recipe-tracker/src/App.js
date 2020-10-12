5edd0c932b734cceb7f414b627366eac452bf0d0
Elijah Heyward <elijah.heyward76@gmail.com>
2020-10-12 16:35:09 -0400

    nothing but struggles

--- recipe-tracker/src/App.js
+++ recipe-tracker/src/App.js
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import DetailedPage from './components/DetailedPage'
+import axios from 'axios';
+// import DetailedPage from './components/DetailedPage'
 import './App.css';
 
 function App() {
+  const { recipes, setRecipes } = useState({});
+
+  useEffect(() => {
+    async function cookIt() {
+      const foodURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/Recipes`
+      const response = await axios.get(foodURL, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
+        }
+      });
+      console.log(response.data.message); 
+    }
+    cookIt();
+  }, []);
+
   return (
     <div className="App">
-     <DetailedPage />
+      {/* <h2>{recipes}</h2> */}
+      {/* <h3></h3>
+      <h3></h3> */}
     </div>
   );
 }
--- recipe-tracker/src/components/DetailedPage.jsx
+++ recipe-tracker/src/components/DetailedPage.jsx
@@ -10,13 +10,17 @@ class DetailedPage extends Component {
     }
   }
   async cookIt() {
-    const foodURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/reviews`;
+    const foodURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/Recipes`;
     const response = await axios.get(foodURL, {
       headers: {
         Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`
       }
     }
-    )}
+      
+    );
+    console.log(response.data);
+    this.setState({ recipes: response.data.records });
+  }
 
   async componentDidMount() {
     await this.cookIt();
