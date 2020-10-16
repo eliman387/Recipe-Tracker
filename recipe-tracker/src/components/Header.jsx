b656f30b49229e007cbe60d2252670828f4359fd
Elijah Heyward <elijah.heyward76@gmail.com>
2020-10-16 14:27:21 -0400

    final fix

--- recipe-tracker/src/components/Header.jsx
+++ recipe-tracker/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import logo from "../logo.png";
+import "../App.css"
 
 class Header extends Component {
   render() {
     return (
       <div>
         <header>
-          <img src={logo} alt="logo" width="150" height="150" />
+          <img
+            id="header-img"
+            src={logo} alt="logo" width="150" height="150" />
           <h1>Recipe Finder Cookbook</h1>
         </header>
       </div>
